ec123fb4fe474e878c0a61ba2183598e92dca469
Gabriel Viseli Barzagli <gabriel.barzagli@hotmail.com>
2017-08-01 01:23:35 -0300

    front-end development

--- embrapa.share.web/src/app/app.component.ts
+++ embrapa.share.web/src/app/app.component.ts
@@ -6,7 +6,7 @@ import { Component } from '@angular/core';
     styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
-    title = 'Share';
+    title = 'Embrapa Share';
 
     links = [
         { name: 'Login', url: 'login' }
